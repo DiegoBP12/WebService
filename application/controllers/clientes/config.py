77fb500ed6fdc540138421211f5b6509b992e993
DiegoBP12 <1717110611@utectulancingo.edu.mx>
2019-03-22 22:12:48 -0600

    Se modificaron los archivos index, master de views/clientes y el archivo config de controllers/clientes

--- application/controllers/clientes/config.py
+++ application/controllers/clientes/config.py
@@ -11,10 +11,10 @@ Here we define the form fields for use in all the views
 '''
 form_clientes= form.Form(
             form.Hidden('id_cliente',description="", value="0", class_="form-control", required=True),
-            form.Textbox('nombre',description="nombre", class_="form-control", required=True),
-            form.Textbox('apellido paterno',description="apellido_paterno", class_="form-control", required=True),
-            form.Textbox('apellido materno',description="apellido materno", class_="form-control", required=True),
-            form.Textbox('telefono',description="telefono", class_="form-control", required=True),
+            form.Textbox('nombre',description="Nombre", class_="form-control", required=True),
+            form.Textbox('apellido_paterno',description="Apellido paterno", class_="form-control", required=True),
+            form.Textbox('apellido_materno',description="Apellido materno", class_="form-control", required=True),
+            form.Textbox('telefono',description="Telefono", class_="form-control", required=True),
             form.Textbox('email', vemail, description="Email", class_="form-control", required=True),
         )
         
