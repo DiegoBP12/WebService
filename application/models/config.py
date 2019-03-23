bbc8d8a8ec5fb0e54a90121fac2a99fbc30b5fd7
DiegoBP12 <1717110611@utectulancingo.edu.mx>
2019-03-23 00:24:22 -0600

    Se modifica el archivo config de models para poder funcionar con servidor remoto

--- application/models/config.py
+++ application/models/config.py
@@ -1,9 +1,14 @@
 import web
 
-db_host = 'localhost'
+"""db_host = 'localhost'
 db_name = 'ferreteria_dbp'
 db_user = 'quetzal'
-db_pw = 'quetzal.2019'
+db_pw = 'quetzal.2019'"""
+
+db_host = 'nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'gsq6mfw949qkkwst'
+db_user = 'z9qsk81jk3dyfuuq'
+db_pw = 'z0wcrkrivvrcm7fu'
 
 db = web.database(
     dbn='mysql',
