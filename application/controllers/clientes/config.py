1c07d0832db9a0ab581ed3b81befa956450318a9
DiegoBP12 <1717110611@utectulancingo.edu.mx>
2019-03-22 23:40:52 -0600

    Modificaciones en el metodo de insertar en los archivos de model_clientes, api_clientes

--- application/controllers/clientes/config.py
+++ application/controllers/clientes/config.py
@@ -10,6 +10,7 @@ vemail = form.regexp(r".*@.*", "Must be a valid email address")
 Here we define the form fields for use in all the views
 '''
 form_clientes= form.Form(
+            form.Textbox('id_cliente',description="Id_cliente", class_="form-control"),
             form.Textbox('nombre',description="Nombre", class_="form-control", required=True),
             form.Textbox('apellido_paterno',description="Apellido paterno", class_="form-control", required=True),
             form.Textbox('apellido_materno',description="Apellido materno", class_="form-control", required=True),
